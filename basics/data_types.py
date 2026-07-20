148aab542746adf28e2cec5c2a2922d8ab075a94
Prakhar <prakh123.singh@gmail.com>
2026-07-20 15:39:12 +0530

    Add data types examples

---
+++ basics/data_types.py
@@ -0,0 +1,45 @@
+# Data Types in Python
+
+# Integer
+age = 20
+print("Integer:", age)
+print(type(age))
+
+# Float
+cgpa = 8.71
+print("\nFloat:", cgpa)
+print(type(cgpa))
+
+# String
+name = "Prakhar Singh"
+print("\nString:", name)
+print(type(name))
+
+# Boolean
+is_student = True
+print("\nBoolean:", is_student)
+print(type(is_student))
+
+# List
+subjects = ["Python", "SQL", "Machine Learning"]
+print("\nList:", subjects)
+print(type(subjects))
+
+# Tuple
+coordinates = (10, 20)
+print("\nTuple:", coordinates)
+print(type(coordinates))
+
+# Set
+colors = {"Red", "Blue", "Green"}
+print("\nSet:", colors)
+print(type(colors))
+
+# Dictionary
+student = {
+    "Name": "Prakhar Singh",
+    "Age": 20,
+    "CGPA": 8.71
+}
+print("\nDictionary:", student)
+print(type(student))
